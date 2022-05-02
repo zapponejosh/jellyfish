979a91ab5082144f0ae32159f47bbe214d79a8a1
Josh Zappone <zappone.josh@gmail.com>
2022-05-02 11:59:35 -0700

    update dependencies

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,5 +4,6 @@ import './globals.css';
 import App from './App';
 
 const container = document.getElementById('root');
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
 root.render(<App />);
