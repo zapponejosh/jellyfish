533599546348125730dbd847fc57a1542a3c2c88
Josh Zappone <zappone.josh@gmail.com>
2022-05-22 16:34:47 -0700

    fix import

--- web/app/pages/Home.tsx
+++ web/app/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import TileGrid from '../components/TileGrid/TileGrid';
-import Welcome from '../components/Welcome/Welcome';
 
 type Project = {
   Name: string;
